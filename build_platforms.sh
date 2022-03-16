3f0580b79fd9ec4d905bc772c30215bdc467c870
Adam McDonagh <adam@elitemonkey.net>
2022-03-16 15:30:05 +0000

    Builds for both RHEL7 & 8

--- build_platforms.sh
+++ build_platforms.sh
@@ -20,7 +20,15 @@ fi
 
 # RHEL platform - Use Alma Linux to simulate RHEL8
 echo "Building for RHEL and Python packages: ${packages}"  
-platform="rhel" test_platforms="almalinux:latest" ./build_and_test_platform.sh ${packages}
+platform="rhel8" test_platforms="almalinux:latest" ./build_and_test_platform.sh ${packages}
+retval=$?
+if [[ $retval -ne 0 ]]; then
+  exit $retval
+fi
+
+# RHEL platform - Use Alma Linux to simulate RHEL7
+echo "Building for RHEL7 and Python packages: ${packages}"  
+platform="rhel7" test_platforms="centos:7" ./build_and_test_platform.sh ${packages}
 retval=$?
 if [[ $retval -ne 0 ]]; then
   exit $retval
