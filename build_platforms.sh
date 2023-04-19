61af25839b7e75b744a6b6292da31edb5bc52d6d
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-04-19 12:09:28 +0000

    First steps at getting workflow working

--- build_platforms.sh
+++ build_platforms.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# export ROOT_CA to a proxy Root CA before running if there's root CA that needs trusting
-test -e root_ca.pem && export ROOT_CA=root_ca.pem
-
 mkdir -p dist
 mkdir -p assets
 mkdir -p scripts
@@ -11,7 +8,7 @@ mkdir -p scripts
 packages=`echo "requests,requests boto3,bofhexcuse" | base64`
 
 # Alpine platform
-echo "Building for Alpine and Python packages: ${packages}"  
+echo "Building for Alpine and Python packages: ${packages}"
 platform="alpine" test_platforms="alpine:latest" ./build_and_test_platform.sh $packages
 retval=$?
 if [[ $retval -ne 0 ]]; then
@@ -19,7 +16,7 @@ if [[ $retval -ne 0 ]]; then
 fi
 
 # RHEL platform - Use Alma Linux to simulate RHEL8
-echo "Building for RHEL and Python packages: ${packages}"  
+echo "Building for RHEL and Python packages: ${packages}"
 platform="rhel8" test_platforms="almalinux:latest" ./build_and_test_platform.sh ${packages}
 retval=$?
 if [[ $retval -ne 0 ]]; then
@@ -27,7 +24,7 @@ if [[ $retval -ne 0 ]]; then
 fi
 
 # RHEL platform - Use Alma Linux to simulate RHEL7
-echo "Building for RHEL7 and Python packages: ${packages}"  
+echo "Building for RHEL7 and Python packages: ${packages}"
 platform="rhel7" test_platforms="centos:7" ./build_and_test_platform.sh ${packages}
 retval=$?
 if [[ $retval -ne 0 ]]; then
@@ -35,7 +32,7 @@ if [[ $retval -ne 0 ]]; then
 fi
 
 # AMZN2 platform - Amazon Linux 2
-echo "Building for RHEL and Python packages: ${packages}"  
+echo "Building for RHEL and Python packages: ${packages}"
 platform="amzn2" test_platforms="amazonlinux:latest" ./build_and_test_platform.sh ${packages}
 retval=$?
 if [[ $retval -ne 0 ]]; then
@@ -43,4 +40,3 @@ if [[ $retval -ne 0 ]]; then
 fi
 
 exit 0
-
